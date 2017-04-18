134cda43150b2472ae52f7f405298e04474a33a0
Keisuke Kawamura <131e55@users.noreply.github.com>
2017-04-18 21:29:26 +0900

    Add AccessToken model

---
+++ Liarodon/Models/AccessToken.swift
@@ -0,0 +1,61 @@
+//
+//  AccessToken.swift
+//  Liarodon
+//
+//  Created by 131e55 on 2017/04/18.
+//  Copyright © 2017年 Liaro Inc. All rights reserved.
+//
+
+import Foundation
+import APIKit
+import Himotoki
+
+struct PostAccessTokenRequest: MastodonRequest {
+
+    let clientID: String
+    let clientSecret: String
+    let userName: String        // User mail address
+    let password: String
+
+    typealias Response = AccessToken
+
+    var method: HTTPMethod {
+        return .post
+    }
+
+    var path: String {
+        return "/oauth/token"
+    }
+
+    var parameters: Any? {
+        return [
+            "client_id": clientID,
+            "client_secret": clientSecret,
+            "grant_type": "password",
+            "username": userName,
+            "password": password
+        ]
+    }
+
+    func response(from object: Any, urlResponse: HTTPURLResponse) throws -> AccessToken {
+        return try AccessToken.decodeValue(object)
+    }
+}
+
+struct AccessToken: Decodable {
+
+    let accessToken: String
+    let createdAt: Int
+    let scope: String
+    let tokenType: String
+
+    static func decode(_ e: Extractor) throws -> AccessToken {
+
+        return try AccessToken(
+            accessToken : e <| "access_token",
+            createdAt   : e <| "created_at",
+            scope       : e <| "scope",
+            tokenType   : e <| "token_type"
+        )
+    }
+}
