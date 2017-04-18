051752265e6e560c9bae43715c2095460181c911
Keisuke Kawamura <131e55@users.noreply.github.com>
2017-04-18 18:30:45 +0900

    Add Xcode project

---
+++ Liarodon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Liarodon
+//
+//  Created by 131e55 on 2017/04/18.
+//  Copyright © 2017年 Liaro Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
