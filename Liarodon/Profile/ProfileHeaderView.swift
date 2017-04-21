158adf28507fb0a652c49f83219feede3e78b683
Keisuke Kawamura <131e55@users.noreply.github.com>
2017-04-21 19:12:29 +0900

    Implement ProfileHeaderView hitTest()

--- Liarodon/Profile/ProfileHeaderView.swift
+++ Liarodon/Profile/ProfileHeaderView.swift
@@ -10,16 +10,17 @@ import UIKit
 
 final class ProfileHeaderView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
     override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        let view = super.hitTest(point, with: event)
+
+        switch view {
+
+        case let button as UIButton:
+            return button
+
+        default:
+            break
+        }
         return nil
     }
-
 }
