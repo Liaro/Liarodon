77686f7b99bc0762b9191fb9e96eadf7fde30c5c
Keisuke Kawamura <131e55@users.noreply.github.com>
2017-04-23 02:38:45 +0900

    Delete print

--- Liarodon/Models/Relationship.swift
+++ Liarodon/Models/Relationship.swift
@@ -39,7 +39,6 @@ final class Relationship {
 extension Relationship: Decodable {
 
     static func decode(_ e: Extractor) throws -> Relationship {
-        print(e)
         return try Relationship(
             id:         e <| "id",
             following:  e <| "following",
